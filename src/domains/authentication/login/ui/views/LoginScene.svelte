6be71552da02c7c5939d42e3ff2ee411cda7aa22
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2026-03-31 02:05:57 +0900

    feat: [이재림] 로그인 화면 UI 구현 [ETH-16]
    - 로그인 카드(로그인 창) 레이아웃 및 blur 효과 적용
    - 타이틀 및 로그인 텍스트 스타일 적용
    - 소셜 로그인 버튼 UI 구성 (Google / Kakao / Naver)
    - 버튼 hover / active 인터랙션 추가
    - 회원가입 안내 및 링크 스타일 적용

---
+++ src/domains/authentication/login/ui/views/LoginScene.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import '../layout/baseFrame.css';
+    import loginBackground from '$lib/assets/login_background.webp';
+    import '../effects/loginBackground.css';
+    import LoginContent from './LoginContent.svelte';
+</script>
+
+<style>
+    .scene {
+        width: 100%;
+        height: 100vh;
+        background-color: #030303;
+        display: flex;
+        flex-direction: column;
+        gap: 120px;
+    }
+
+    .content {
+        position: relative;
+        z-index: 3;
+
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
+<div class="frame scene">
+    <!-- 배경 -->
+    <div
+        class="frame-bg"
+        style={`background-image: url(${loginBackground});`}>
+    </div>
+
+    <!-- 배경 효과 적용-->
+    <div class="overlay-dark"></div>
+    <div class="overlay-vignette"></div>
+
+    <!-- 로그인 창-->
+    <div class="content">
+        <LoginContent />
+    </div>
+</div>
